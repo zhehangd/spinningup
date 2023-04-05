9603e513b9e53b4a52fbdb6cb4104e2ecd733002
Zhehang Ding <zhehangd@gmail.com>
2023-04-04 23:28:22 -0700

    Fix ppo calls to gym envs

--- spinup/algos/pytorch/ppo/ppo.py
+++ spinup/algos/pytorch/ppo/ppo.py
@@ -292,14 +292,17 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
     # Prepare for interaction with environment
     start_time = time.time()
-    o, ep_ret, ep_len = env.reset(), 0, 0
+    ep_ret = 0
+    ep_len = 0
+    o, _ = env.reset() # observation, info
 
     # Main loop: collect experience in env and update/log each epoch
     for epoch in range(epochs):
         for t in range(local_steps_per_epoch):
             a, v, logp = ac.step(torch.as_tensor(o, dtype=torch.float32))
 
-            next_o, r, d, _ = env.step(a)
+            # observation, reward, terminated, truncated, info
+            next_o, r, d, _, _ = env.step(a)
             ep_ret += r
             ep_len += 1
 
@@ -326,7 +329,9 @@ def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                 if terminal:
                     # only save EpRet / EpLen if trajectory finished
                     logger.store(EpRet=ep_ret, EpLen=ep_len)
-                o, ep_ret, ep_len = env.reset(), 0, 0
+                ep_ret = 0
+                ep_len = 0
+                o, _ = env.reset()
 
 
         # Save model
